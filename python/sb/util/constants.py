fd53ac9f46bfa53f32af2773b4163df750806000
Martin Hammarstedt <martin.hammarstedt@gu.se>
2011-01-04 09:50:14 +0000

    No commit message

--- python/sb/util/constants.py
+++ python/sb/util/constants.py
@@ -102,4 +102,30 @@ LEMMA = 'lemma'
 LEX = 'lex'
 PRECISION = 'precision'
 
-
+COLORS = {
+    "default"    :    "\033[m",
+    # Styles
+    "bold"       :    "\033[1m",
+    "underline"  :    "\033[4m",
+    "blink"      :    "\033[5m",
+    "reverse"    :    "\033[7m",
+    "concealed"  :    "\033[8m",
+    # Font colors
+    "black"      :    "\033[30m", 
+    "red"        :    "\033[31m",
+    "green"      :    "\033[32m",
+    "yellow"     :    "\033[33m",
+    "blue"       :    "\033[34m",
+    "magenta"    :    "\033[35m",
+    "cyan"       :    "\033[36m",
+    "white"      :    "\033[37m",
+    # Background colors
+    "on_black"   :    "\033[40m", 
+    "on_red"     :    "\033[41m",
+    "on_green"   :    "\033[42m",
+    "on_yellow"  :    "\033[43m",
+    "on_blue"    :    "\033[44m",
+    "on_magenta" :    "\033[45m",
+    "on_cyan"    :    "\033[46m",
+    "on_white"   :    "\033[47m" 
+} 
