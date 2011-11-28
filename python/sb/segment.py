62e8d1065576c07d2e8a6afcdb59ee2e2cae2c01
Martin Hammarstedt <martin.hammarstedt@gu.se>
2011-11-28 11:32:55 +0000

    No commit message

--- python/sb/segment.py
+++ python/sb/segment.py
@@ -72,8 +72,8 @@ class ModifiedLanguageVars(nltk.tokenize.punkt.PunktLanguageVars):
     punctuation."""
     # http://nltk.googlecode.com/svn/trunk/doc/api/nltk.tokenize.punkt.PunktLanguageVars-class.html
     # http://nltk.googlecode.com/svn/trunk/doc/api/nltk.tokenize.punkt-pysrc.html#PunktLanguageVars
-    _re_word_start = ur'''[^\(\"\'–—“”»\`\\{\/\[:;&\#\*@\)}\]\-,…]'''
-    _re_non_word_chars = ur'(?:[?!)\"“”»–—\\;\/}\]\*:\'\({\[…])' #@
+    _re_word_start = ur'''[^\(\"\'‘’–—“”»\`\\{\/\[:;&\#\*@\)}\]\-,…]'''
+    _re_non_word_chars = ur'(?:[?!)\"“”»–—\\;\/}\]\*:\'‘’\({\[…%])' #@
     re_boundary_realignment = re.compile(ur'[“”"\')\]}]+?(?:\s+|(?=--)|$)',
             re.MULTILINE)
 
