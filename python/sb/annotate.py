46bf4b1a90c56d96a0c3bd663fa30dd99e1555dc
Martin Hammarstedt <martin.hammarstedt@gu.se>
2012-10-03 11:23:37 +0000

    No commit message

--- python/sb/annotate.py
+++ python/sb/annotate.py
@@ -84,9 +84,10 @@ def find_replace(chunk, out, find, sub=""):
     """Find and replace parts of or whole annotation."""
     util.write_annotation(out, ((key, val.replace(find, sub)) for (key, val) in util.read_annotation_iteritems(chunk)))
 
-def concat(out, left, right, separator="", encoding=util.UTF8):
+def concat(out, left, right, separator="", merge_twins=False, encoding=util.UTF8):
+    """Concatenate values from two annotations, with an optional separator."""
     b = util.read_annotation(right)
-    util.write_annotation(out, ((key_a, u"%s%s%s" % (val_a, separator.decode(encoding), b[key_a])) for (key_a, val_a) in util.read_annotation_iteritems(left)))
+    util.write_annotation(out, ((key_a, u"%s%s%s" % (val_a, separator.decode(encoding), b[key_a]) if not (merge_twins and val_a == b[key_a]) else val_a) for (key_a, val_a) in util.read_annotation_iteritems(left)))
     
 
 if __name__ == '__main__':
