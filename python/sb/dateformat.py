1059685864f9d65fc449de0d8d6f9fa02b49c196
Martin Hammarstedt <martin.hammarstedt@gu.se>
2012-11-07 14:21:41 +0000

    Stöd för skottår innan 1900

--- python/sb/dateformat.py
+++ python/sb/dateformat.py
@@ -201,10 +201,12 @@ def dateformat(infrom, outfrom=None, into=None, outto=None, informat="", outform
 def strftime(dt, fmt):
     """Python datetime.strftime < 1900 workaround, taken from https://gist.github.com/2000837"""
 
+    TEMPYEAR = 9996
+
     if dt.year < 1900:
         # create a copy of this datetime, just in case, then set the year to
         # something acceptable, then replace that year in the resulting string
-        tmp_dt = datetime.datetime(datetime.MAXYEAR, dt.month, dt.day,
+        tmp_dt = datetime.datetime(TEMPYEAR, dt.month, dt.day,
                                   dt.hour, dt.minute,
                                   dt.second, dt.microsecond,
                                   dt.tzinfo)
@@ -215,19 +217,19 @@ def strftime(dt, fmt):
         tmp_fmt = fmt
         tmp_fmt = re.sub('(?<!%)((?:%%)*)(%y)', '\\1\x11\x11', tmp_fmt, re.U)
         tmp_fmt = re.sub('(?<!%)((?:%%)*)(%Y)', '\\1\x12\x12\x12\x12', tmp_fmt, re.U)
-        tmp_fmt = tmp_fmt.replace(str(datetime.MAXYEAR), '\x13\x13\x13\x13')
-        tmp_fmt = tmp_fmt.replace(str(datetime.MAXYEAR)[-2:], '\x14\x14')
+        tmp_fmt = tmp_fmt.replace(str(TEMPYEAR), '\x13\x13\x13\x13')
+        tmp_fmt = tmp_fmt.replace(str(TEMPYEAR)[-2:], '\x14\x14')
         
         result = tmp_dt.strftime(tmp_fmt)
         
         if '%c' in fmt:
             # local datetime format - uses full year but hard for us to guess where.
-            result = result.replace(str(datetime.MAXYEAR), str(dt.year))
+            result = result.replace(str(TEMPYEAR), str(dt.year))
         
         result = result.replace('\x11\x11', str(dt.year)[-2:])
         result = result.replace('\x12\x12\x12\x12', str(dt.year))
-        result = result.replace('\x13\x13\x13\x13', str(datetime.MAXYEAR))
-        result = result.replace('\x14\x14', str(datetime.MAXYEAR)[-2:])
+        result = result.replace('\x13\x13\x13\x13', str(TEMPYEAR))
+        result = result.replace('\x14\x14', str(TEMPYEAR)[-2:])
             
         return result
         
