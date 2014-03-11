74bd00e8cda1117fc4ea7ac027a5e47726e43be0
Martin Hammarstedt <martin.hammarstedt@gu.se>
2014-03-11 12:32:42 +0000

    Snyggar till
    Former-commit-id: bcf3b8a899895946eb0cfd183fba9fb4c51094d2

--- python/sb/lmflexicon.py
+++ python/sb/lmflexicon.py
@@ -10,9 +10,6 @@ Does handle multiwords expressions with gaps
 
 import sb.saldo as s
 import util
-import itertools
-import cPickle as pickle
-import re
 
 ######################################################################
 # converting between different file formats
@@ -30,7 +27,7 @@ def read_xml(xml='dalinm.xml', annotation_elements='writtenForm lemgram', verbos
     import re
     if verbose: util.log.info("Reading XML lexicon")
     lexicon = {}
-    
+
     context = cet.iterparse(xml, events=("start", "end")) # "start" needed to save reference to root element
     context = iter(context)
     event, root = context.next()
@@ -39,57 +36,57 @@ def read_xml(xml='dalinm.xml', annotation_elements='writtenForm lemgram', verbos
         if event == "end":
             if elem.tag == 'LexicalEntry':
                 annotations = s.hashabledict()
-                
+
                 lem = elem.find("Lemma").find("FormRepresentation")
                 for a in annotation_elements:
-                    annotations[a] = tuple([findval(lem,a)])
-                        
-                pos = findval(lem,"partOfSpeech")
-                
+                    annotations[a] = tuple([findval(lem, a)])
+
+                pos = findval(lem, "partOfSpeech")
+
                 # there may be several WordForms
-                for forms in elem.findall("WordForm"): 
-                  word  = findval(forms,"writtenForm")
-                  param = findval(forms,"msd")
+                for forms in elem.findall("WordForm"):
+                    word  = findval(forms, "writtenForm")
+                    param = findval(forms, "msd")
 
-                  multiwords = []
-                  wordparts = word.split()
-                  for i,word in enumerate(wordparts):
-                    if (not skip_multiword) and len(wordparts)>1:
+                    multiwords = []
+                    wordparts = word.split()
+                    for i, word in enumerate(wordparts):
+                        if (not skip_multiword) and len(wordparts) > 1:
 
-                        # Handle multi-word expressions
-                        multiwords.append(word)
+                            # Handle multi-word expressions
+                            multiwords.append(word)
 
-                        particle = False # we don't use any particles or mwe:s with gaps
-                        mwe_gap  = False # but keep the fields so that the file format match the normal saldo-pickle format
-                        
-                        # is it the last word in the multi word expression?
-                        if i==len(wordparts)-1:
-                            lexicon.setdefault(multiwords[0], {}).setdefault(annotations, (set(), set(), mwe_gap, particle))[1].add(tuple(multiwords[1:]))
-                            multiwords = []
-                    else:
-                        # Single word expressions
-                        saldotag = " ".join([pos,param]) # this tag is rather useless, but at least gives some information
-                        tags = tuple([saldotag])
-                        lexicon.setdefault(word, {}).setdefault(annotations, (set(), set(), mwe_gap, particle))[0].update(tags)
+                            particle = False # we don't use any particles or mwe:s with gaps
+                            mwe_gap  = False # but keep the fields so that the file format match the normal saldo-pickle format
+
+                            # is it the last word in the multi word expression?
+                            if i == len(wordparts) - 1:
+                                lexicon.setdefault(multiwords[0], {}).setdefault(annotations, (set(), set(), mwe_gap, particle))[1].add(tuple(multiwords[1:]))
+                                multiwords = []
+                        else:
+                            # Single word expressions
+                            saldotag = " ".join([pos, param]) # this tag is rather useless, but at least gives some information
+                            tags = tuple([saldotag])
+                            lexicon.setdefault(word, {}).setdefault(annotations, (set(), set(), mwe_gap, particle))[0].update(tags)
 
             # Done parsing section. Clear tree to save memory
             if elem.tag in ['LexicalEntry', 'frame', 'resFrame']:
                 root.clear()
     if verbose:
-      s.test_annotations(lexicon)
-      util.log.info("OK, read")
+        s.test_annotations(lexicon)
+        util.log.info("OK, read")
     return lexicon
-    
-def findval(elems,key):
-  def iterfindval():
-    for form in elems:
-      att = form.get("att","")
-      if att==key:
-        yield form.get("val")
-    yield ""
 
-  return iterfindval().next()
-  
+def findval(elems, key):
+    def iterfindval():
+        for form in elems:
+            att = form.get("att", "")
+            if att == key:
+                yield form.get("val")
+        yield ""
+
+    return iterfindval().next()
+
 
 ######################################################################
 # additional utilities
@@ -104,14 +101,13 @@ testwords = [u"äggtoddyarna",
              u"pittoresk",
              u"in"]
 
-
-def xml_to_pickle(xml, filename, annotation_elements="writtenForm lemgram",skip_multiword=True):
+def xml_to_pickle(xml, filename, annotation_elements="writtenForm lemgram", skip_multiword=True):
     """Read an XML dictionary and save as a pickle file."""
-    
-    xml_lexicon = read_xml(xml, annotation_elements,skip_multiword=skip_multiword)
+
+    xml_lexicon = read_xml(xml, annotation_elements, skip_multiword=skip_multiword)
     s.SaldoLexicon.save_to_picklefile(filename, xml_lexicon)
 
 ######################################################################
 
 if __name__ == '__main__':
-    util.run.main(annotate, xml_to_pickle=xml_to_pickle)
+    util.run.main(xml_to_pickle=xml_to_pickle)
