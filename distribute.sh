0e7bb3071fde38ac94aff18c54d37ebabdf08c61
Anne Schumacher <anne.schumacher@gu.se>
2017-03-24 09:53:37 +0000

    bug fixes in distribution script
    Former-commit-id: f9d05b17e0f6e8eda1b79c6fc09cd87905bb7362

--- distribute.sh
+++ distribute.sh
@@ -5,16 +5,19 @@
 # The version with MIT license removes all references to freeling.
 # This requires that the affected code lines are marked with a "FreeLing" comment.
 
+# user fksparv cannot access SVN repo :(
+USER=anne
+
 DATE=`date +%Y-%m-%d`
 # Extract version number from first line in file 'VERSION'
-line=$(head -n 1 annotate/VERSION)
+line=$(head -n 1 VERSION)
 VERSION=${line#"version: "}
 DIR_MIT=sparv_backend\_$VERSION\_MIT\_$DATE
 DIR_AGPL=sparv_backend\_$VERSION\_AGPL\_$DATE
 
 # export tools/annotate and catapult & zip content
 echo -e "Distributing current version under MIT license!\n"
-ssh anne@k2.spraakdata.gu.se "cd /export/htdocs_sb/pub/sparv.dist/sparv_pipeline;
+ssh $USER@k2.spraakdata.gu.se "cd /export/htdocs_sb/pub/sparv.dist/sparv_pipeline;
 if [ -f $DIR_MIT.zip ]; then echo -e 'File $DIR_MIT.zip already exists!\n'; exit; fi;
 mkdir $DIR_MIT && cd $DIR_MIT;
 echo 'Exporting sources from SVN...';
@@ -34,29 +37,28 @@ rm annotate/SB_README.txt;
 rm annotate/models/saldom.xml;
 rm annotate/models/saldo.pickle;
 rm annotate/models/saldo.compound.pickle;
-rm annotate/models/saldo.compound.pickle;
 rm annotate/models/hunpos.saldo.suc-tags.morphtable;
 rm annotate/models/stats.pickle;
 rm annotate/models/bettertokenizer.sv.saldo-tokens;
-echo 'Removing references to FreeLing';
-for f in $(find ./annotate/makefiles -type f); do sed -i '/# FreeLing/d' $f; done;
-for f in $(find ./annotate/python -type f); do sed -i '/FreeLing/d' $f; done;
+echo 'Removing references to Freeling';
 rm annotate/python/sb/freeling.py;
 rm -rf annotate/models/freeling;
 rm annotate/models/treetagger/*;
 rm annotate/bin/treetagger/*;
 rm annotate/bin/word_alignment/*;
+for f in \$(find ./annotate/makefiles -type f); do sed -i '/# FreeLing/d' \$f; done;
+for f in \$(find ./annotate/python -type f); do sed -i '/FreeLing/d' \$f; done;
 
 echo 'zipping files...';
 cd ..;
 zip -rq $DIR_MIT.zip $DIR_MIT;
 rm -r $DIR_MIT;
-if [ -f $DIR_MIT.zip ]; then echo -e 'Distribution successful!\n'; else echo -e 'Something went wrong...\n'; fi;"
+if [ -f $DIR_MIT.zip ]; then echo -e 'Distribution with MIT license successful!\n'; else echo -e 'Something went wrong...\n'; fi;"
 
 
 # export tools/annotate and catapult & zip content
-echo -e "Distributing current version under MIT license!\n"
-ssh anne@k2.spraakdata.gu.se "cd /export/htdocs_sb/pub/sparv.dist/sparv_pipeline;
+echo -e "Distributing current version under AGPL license!\n"
+ssh $USER@k2.spraakdata.gu.se "cd /export/htdocs_sb/pub/sparv.dist/sparv_pipeline;
 if [ -f $DIR_AGPL.zip ]; then echo -e 'File $DIR_AGPL.zip already exists!\n'; exit; fi;
 mkdir $DIR_AGPL && cd $DIR_AGPL;
 echo 'Exporting sources from SVN...';
@@ -76,7 +78,6 @@ rm annotate/SB_README.txt;
 rm annotate/models/saldom.xml;
 rm annotate/models/saldo.pickle;
 rm annotate/models/saldo.compound.pickle;
-rm annotate/models/saldo.compound.pickle;
 rm annotate/models/hunpos.saldo.suc-tags.morphtable;
 rm annotate/models/stats.pickle;
 rm annotate/models/bettertokenizer.sv.saldo-tokens;
@@ -89,4 +90,4 @@ echo 'zipping files...';
 cd ..;
 zip -rq $DIR_AGPL.zip $DIR_AGPL;
 rm -r $DIR_AGPL;
-if [ -f $DIR_AGPL.zip ]; then echo -e 'Distribution successful!\n'; else echo -e 'Something went wrong...\n'; fi;"
+if [ -f $DIR_AGPL.zip ]; then echo -e 'Distribution with AGPL license successful!\n'; else echo -e 'Something went wrong...\n'; fi;"
