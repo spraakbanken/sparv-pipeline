b445615324f8d8888ccc7ec29fd33882c95863e1
Anne Schumacher <anne.schumacher@gu.se>
2017-09-07 11:14:24 +0000

    added support for sentiment analysis
    Former-commit-id: 67f82df72cea543d0aba6197beb2a4ad6326cd65

--- python/sb/lexical_classes.py
+++ python/sb/lexical_classes.py
@@ -3,29 +3,40 @@ import sb.util as util
 import pickle
 
 
-def annotate_bb_words(out_blingbring, model, saldoids, delimiter="|", affix="|", lexicon=None):
+def annotate_bb_words(out, model, saldoids, delimiter=util._DELIM, affix=util._AFFIX, lexicon=None):
+    """Blingbring wrapper for annotate_words. Annotations words with blingbring classes (rogetID)."""
+    annotate_words(out, model, saldoids, get_blingbring, delimiter, affix, lexicon=None)
+
+
+def annotate_sent_words(out, model, saldoids, delimiter=util._DELIM, affix=util._AFFIX, lexicon=None):
+    """Sentiment wrapper for annotate_words. Annotations words with sentiments."""
+    annotate_words(out, model, saldoids, get_sentiment, delimiter, affix, lexicon=None)
+
+
+def annotate_words(out, model, saldoids, annotation, delimiter, affix, lexicon=None):
     """
-    Annotate words with blingbring classes (rogetID).
-    - out_blingbring: resulting annotation file
-    - model: pickled blingbring lexicon.
+    Annotate words with model and annotation function.
+    - out_sent: resulting annotation file.
+    - model: pickled lexicon with saldoIDs as keys.
     - saldoids: existing annotation with saldoIDs.
+    - annotation: annotation function (get_blingbring or get_sentiment)
     - delimiter: delimiter character to put between ambiguous results
     - affix: optional character to put before and after results to mark a set.
     - lexicon: this argument cannot be set from the command line,
       but is used in the catapult. This argument must be last.
     """
     if not lexicon:
-        lexicon = Blingbring(model)
+        lexicon = util.PickledLexicon(model)
     # Otherwise use pre-loaded lexicon (from catapult)
 
-    OUT_blingbring = {}
+    OUT = {}
 
     SALDO_ID = util.read_annotation(saldoids)
     for tokid in SALDO_ID:
-        rogetid = set()
-        if ":" in SALDO_ID[tokid]:  # WSD
-            ranked_saldo = SALDO_ID[tokid].strip("|").split("|") if SALDO_ID[tokid] != "|" else None
-            saldo_tuples = [(i.split(":")[0], i.split(":")[1]) for i in ranked_saldo]
+        if util._SCORESEP in SALDO_ID[tokid]:  # WSD
+            ranked_saldo = SALDO_ID[tokid].strip(util._AFFIX).split(util._DELIM) \
+                if SALDO_ID[tokid] != util._AFFIX else None
+            saldo_tuples = [(i.split(util._SCORESEP)[0], i.split(util._SCORESEP)[1]) for i in ranked_saldo]
 
             # Handle wsd with equal probability for several words
             saldo_ids = [saldo_tuples[0]]
@@ -37,21 +48,81 @@ def annotate_bb_words(out_blingbring, model, saldoids, delimiter="|", affix="|",
             saldo_ids = [i[0] for i in saldo_ids]
 
         else:  # No WSD
-            saldo_ids = SALDO_ID[tokid].strip("|").split("|") if SALDO_ID[tokid] != "|" else None
+            saldo_ids = SALDO_ID[tokid].strip(util._AFFIX).split(util._DELIM) \
+                if SALDO_ID[tokid] != util._AFFIX else None
 
-        if saldo_ids:
-            for sid in saldo_ids:
-                rogetid = rogetid.union(lexicon.lookup(sid))
+        result = annotation(saldo_ids, lexicon)
 
-        OUT_blingbring[tokid] = util.cwbset(sorted(rogetid), delimiter, affix) if rogetid else affix
-    util.write_annotation(out_blingbring, OUT_blingbring)
+        OUT[tokid] = util.cwbset(result, delimiter, affix) if result else affix
+    util.write_annotation(out, OUT)
 
 
-def annotate_bb_document(out, in_bb_word, text, cutoff=10, delimiter="|", affix="|"):
+def get_blingbring(saldo_ids, lexicon):
+    rogetid = set()
+    if saldo_ids:
+        for sid in saldo_ids:
+            rogetid = rogetid.union(lexicon.lookup(sid, default=set()))
+    return sorted(rogetid)
+
+
+def get_sentiment(saldo_ids, lexicon):
+    sents = {}
+    if saldo_ids:
+        for sid in saldo_ids:
+            sentiment = lexicon.lookup(sid, default=None)
+            if sentiment:
+                sents[sentiment[0]] = sents.setdefault(sentiment[0], 0) + 1
+
+        if sents:
+            if len(sents) > 1:
+                sents = sorted(sents.items(), key=lambda x: x[1], reverse=True)
+                sents = [util._SCORESEP.join([sent, str(freq)]) for sent, freq in sents]
+            else:
+                sents = [sorted(sents.items())[0][0]]
+    return sorted(sents)
+
+
+def annotate_sent_doc(out, in_token_sent, text, delimiter=util._DELIM, affix=util._AFFIX):
+    """
+    Annotate documents with sentiments.
+    - out: resulting annotation file
+    - in_token_sent: existing annotation with blingbring tokens.
+    - text: existing annotation file for text parent.
+    - delimiter: delimiter character to put between ambiguous results.
+    - affix: optional character to put before and after results to mark a set.
+    """
+    in_token = util.read_annotation(in_token_sent)
+    sentiment_freqs = {}
+
+    for tokid in in_token:
+        sentiments = in_token[tokid].strip(util._AFFIX).split(util._DELIM) \
+            if in_token[tokid] != util._AFFIX else []
+        if sentiments:
+            for sent in sentiments:
+                if util._SCORESEP in sent:  # Sentiment contains frequency
+                    s, f = sent.split(util._SCORESEP)
+                    sentiment_freqs[s] = sentiment_freqs.setdefault(s, 0) + int(f)
+                else:
+                    sentiment_freqs[sent] = sentiment_freqs.setdefault(sent, 0) + 1
+
+    # Sort dictionary and join tuples with words and frequencies
+    sentiment_freqs = sorted(sentiment_freqs.items(), key=lambda x: x[1], reverse=True)
+    sentiment_freqs = [util._SCORESEP.join([sent, str(freq)]) for sent, freq in sentiment_freqs]
+
+    # Write annotation on text level
+    text = util.read_annotation(text)
+    out_sentiments = {}
+    for tokid in text:
+        out_sentiments[tokid] = util.cwbset(sentiment_freqs, delimiter, affix) if sentiment_freqs else affix
+
+    util.write_annotation(out, out_sentiments)
+
+
+def annotate_bb_doc(out, in_token_bb, text, cutoff=10, delimiter=util._DELIM, affix=util._AFFIX):
     """
     Annotate documents with blingbring classes (rogetID).
     - out: resulting annotation file
-    - in_bb_word: pickled blingbring lexicon.
+    - in_token_bb: existing annotation with blingbring tokens.
     - text: existing annotation file for text parent.
     - cutoff: value for limiting the resulting bring classes.
               The result will contain all words with the top x frequencies.
@@ -60,15 +131,14 @@ def annotate_bb_document(out, in_bb_word, text, cutoff=10, delimiter="|", affix=
     - affix: optional character to put before and after results to mark a set.
     """
     cutoff = int(cutoff)
-    OUT_bb_doc = {}
 
-    roget_words = util.read_annotation(in_bb_word)
-    text = util.read_annotation(text)
+    roget_words = util.read_annotation(in_token_bb)
 
     roget_freqs = {}
 
     for tokid in roget_words:
-        words = roget_words[tokid].strip("|").split("|") if roget_words[tokid] != "|" else []
+        words = roget_words[tokid].strip(util._AFFIX).split(util._DELIM) \
+            if roget_words[tokid] != util._AFFIX else []
         for w in words:
             roget_freqs[w] = roget_freqs.setdefault(w, 0) + 1
 
@@ -81,31 +151,19 @@ def annotate_bb_document(out, in_bb_word, text, cutoff=10, delimiter="|", affix=
         ordered_words = [w for w in ordered_words if w[1] >= cutoff_freq]
 
     # Join tuples with words and frequencies
-    ordered_words = [":".join([word, str(freq)]) for word, freq in ordered_words]
+    ordered_words = [util._SCORESEP.join([word, str(freq)]) for word, freq in ordered_words]
     # util.log.info("%s", ordered_words)
 
+    # Write annotation on text level
+    text = util.read_annotation(text)
+    out_bb_doc = {}
     for tokid in text:
-        OUT_bb_doc[tokid] = util.cwbset(ordered_words, delimiter, affix) if ordered_words else affix
+        out_bb_doc[tokid] = util.cwbset(ordered_words, delimiter, affix) if ordered_words else affix
 
-    util.write_annotation(out, OUT_bb_doc)
-
-
-class Blingbring(object):
-    """Lexicon for Blingbring lookups, initialized from a Pickled file."""
-    def __init__(self, picklefile, verbose=True):
-        if verbose:
-            util.log.info("Reading Blingbring lexicon: %s", picklefile)
-        with open(picklefile, "rb") as F:
-            self.lexicon = pickle.load(F)
-        if verbose:
-            util.log.info("OK, read %d words", len(self.lexicon))
-
-    def lookup(self, saldo_id):
-        """Lookup a word in the lexicon."""
-        return self.lexicon.get(saldo_id, set())
+    util.write_annotation(out, out_bb_doc)
 
 
-def read_xml(xml='blingbring.xml', verbose=True):
+def read_blingbring(xml='blingbring.xml', verbose=True):
     """
     Read the XML version of the Blingbring lexicon (blingbring.xml).
     Return a lexicon dictionary: {senseid: set([rogetID, rogetID ...])}
@@ -145,9 +203,49 @@ def read_xml(xml='blingbring.xml', verbose=True):
     return lexicon
 
 
-def xml_to_pickle(xml, filename, protocol=-1, verbose=True):
-    """Read an XML dictionary and save as a pickle file."""
-    lexicon = read_xml(xml)
+def read_sensaldo(tsv="sensaldo_provisional.txt", verbose=True):
+    """
+    Read the TSV version of the sensaldo lexicon (sensaldo.txt).
+    Return a lexicon dictionary: {senseid: (sentiment, ranking)}
+    """
+    import csv
+
+    if verbose:
+        util.log.info("Reading TSV lexicon")
+    lexicon = {}
+
+    with open(tsv) as f:
+        for line in csv.reader(f, delimiter="\t"):
+            saldoid = line[0]
+            sentiment = line[1]
+            ranking = line[2]
+            lexicon[saldoid] = (sentiment, ranking)
+
+    testwords = ["hemsk..1",
+                 "terrier..1",
+                 "festlig..2"
+                 ]
+    util.test_annotations(lexicon, testwords)
+
+    if verbose:
+        util.log.info("OK, read")
+    return lexicon
+
+
+def blingbring_to_pickle(xml, filename, protocol=-1, verbose=True):
+    """Read blingbring xml dictionary and save as a pickle file."""
+    lexicon = read_blingbring(xml)
+    lexicon_to_pickle(xml, filename, lexicon)
+
+
+def sensaldo_to_pickle(tsv, filename, protocol=-1, verbose=True):
+    """Read sensaldo tsv dictionary and save as a pickle file."""
+    lexicon = read_sensaldo(tsv)
+    lexicon_to_pickle(tsv, filename, lexicon)
+
+
+def lexicon_to_pickle(lexicon, filename, protocol=-1, verbose=True):
+    """Save lexicon as a pickle file."""
     if verbose:
         util.log.info("Saving lexicon in pickle format")
     with open(filename, "wb") as F:
@@ -158,5 +256,9 @@ def xml_to_pickle(xml, filename, protocol=-1, verbose=True):
 
 if __name__ == '__main__':
     util.run.main(annotate_bb_words=annotate_bb_words,
-                  annotate_bb_document=annotate_bb_document,
-                  xml_to_pickle=xml_to_pickle)
+                  annotate_bb_doc=annotate_bb_doc,
+                  annotate_sent_words=annotate_sent_words,
+                  annotate_sent_doc=annotate_sent_doc,
+                  blingbring_to_pickle=blingbring_to_pickle,
+                  sensaldo_to_pickle=sensaldo_to_pickle
+                  )
