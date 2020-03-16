7d199fc23b69bade6dba898bb98ccdb5bfdcf95a
Martin Hammarstedt <martin.hammarstedt@gu.se>
2020-03-16 14:35:12 +0100

    Use -q flag with cwb-scan-corpus in timespan.py

--- sparv/modules/korp/timespan.py
+++ sparv/modules/korp/timespan.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import subprocess
 import os
 import sparv.util as util
@@ -15,30 +14,32 @@ def timespan(corpus, db_name, out):
     def calculate(usetime=True):
         rows = []
 
-        dateattribs = ["text_datefrom", "text_timefrom", "text_dateto", "text_timeto"] if usetime else ["text_datefrom", "text_dateto"]
+        dateattribs = ["text_datefrom", "text_timefrom", "text_dateto", "text_timeto"] if usetime else ["text_datefrom",
+                                                                                                        "text_dateto"]
 
-        process = subprocess.Popen([CWB_SCAN_EXECUTABLE, "-r", CORPUS_REGISTRY, corpus] + dateattribs, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process = subprocess.Popen([CWB_SCAN_EXECUTABLE, "-q", "-r", CORPUS_REGISTRY, corpus] + dateattribs,
+                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         reply, error = process.communicate()
         reply = reply.decode()
         if error:
             error = error.decode()
-            if "Error:" in error:  # We always get something back on stderror from cwb-scan-corpus, so we must check if it really is an error
-                if "Error: can't open attribute" in error and (".text_datefrom" in error or ".text_dateto" in error):
-                    util.log.info("No date information present in corpus.")
-                    # No date information in corpus. Calculate total token count instead.
-                    process = subprocess.Popen([CQP_EXECUTABLE, "-c", "-r", CORPUS_REGISTRY], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                    reply, error = process.communicate(bytes("set PrettyPrint off;%s;info;" % corpus, "UTF-8"))
+            if "Error: can't open attribute" in error and (".text_datefrom" in error or ".text_dateto" in error):
+                util.log.info("No date information present in corpus.")
+                # No date information in corpus. Calculate total token count instead.
+                process = subprocess.Popen([CQP_EXECUTABLE, "-c", "-r", CORPUS_REGISTRY], stdin=subprocess.PIPE,
+                                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                reply, error = process.communicate(bytes("set PrettyPrint off;%s;info;" % corpus, "UTF-8"))
 
-                    if error:
-                        print(error)
-                        raise Exception
-
-                    for line in reply.decode().splitlines():
-                        if line.startswith("Size: "):
-                            reply = "%s\t\t\t\t" % line[6:].strip()
-                else:
+                if error:
                     print(error)
                     raise Exception
+
+                for line in reply.decode().splitlines():
+                    if line.startswith("Size: "):
+                        reply = "%s\t\t\t\t" % line[6:].strip()
+            else:
+                print(error)
+                raise Exception
 
         spans = {}
 
@@ -91,30 +92,31 @@ def timespan(corpus, db_name, out):
     mysql.add_row(MYSQL_TABLE, rows_datetime)
     mysql.add_row(MYSQL_TABLE_DATE, rows_date)
 
+
 ################################################################################
 
 MYSQL_TABLE = "timedata"
 MYSQL_TABLE_DATE = "timedata_date"
 
-MYSQL_TIMESPAN = {'columns': [
+MYSQL_TIMESPAN = {"columns": [
                                ("corpus",   "varchar(64)", "", "NOT NULL"),
                                ("datefrom",  "datetime", "0000-00-00 00:00:00", "NOT NULL"),
                                ("dateto",  "datetime", "0000-00-00 00:00:00", "NOT NULL"),
                                ("tokens",   int, 0, "NOT NULL")],
-                  'primary': "corpus datefrom dateto",
-                  'indexes': [],
-                  'default charset': 'utf8'
+                  "primary": "corpus datefrom dateto",
+                  "indexes": [],
+                  "default charset": "utf8"
                   }
 
-MYSQL_TIMESPAN_DATE = {'columns': [
+MYSQL_TIMESPAN_DATE = {"columns": [
                                ("corpus",   "varchar(64)", "", "NOT NULL"),
                                ("datefrom",  "date", "0000-00-00", "NOT NULL"),
                                ("dateto",  "date", "0000-00-00", "NOT NULL"),
                                ("tokens",   int, 0, "NOT NULL")],
-                       'primary': "corpus datefrom dateto",
-                       'indexes': [],
-                       'default charset': 'utf8'
+                       "primary": "corpus datefrom dateto",
+                       "indexes": [],
+                       "default charset": "utf8"
                        }
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     util.run.main(timespan)
