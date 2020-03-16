34bab83b708a8d883044401c58f8bbcacca68d0b
Martin Hammarstedt <martin.hammarstedt@gu.se>
2020-03-16 14:23:59 +0100

    Update swener.py to use the new annotation format

--- sparv/modules/swener/swener.py
+++ sparv/modules/swener/swener.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import re
 import xml.sax.saxutils
 import xml.etree.cElementTree as etree
@@ -9,7 +8,7 @@ SENT_SEP = "\n"
 TOK_SEP = " "
 
 
-def tag_ne(out_ne_ex, out_ne_type, out_ne_subtype, out_ne_name, word, sentence, text, token, encoding=util.UTF8, process_dict=None):
+def tag_ne(doc, out_ne, out_ne_ex, out_ne_type, out_ne_subtype, out_ne_name, word, sentence, token, process_dict=None):
     """
     Tag named entities using HFST-SweNER.
     SweNER is either run in an already started process defined in
@@ -20,22 +19,21 @@ def tag_ne(out_ne_ex, out_ne_type, out_ne_subtype, out_ne_name, word, sentence, 
     """
 
     if process_dict is None:
-        process = swenerstart("", encoding, verbose=False)
+        process = swenerstart("", util.UTF8, verbose=False)
     # else:
-    #     process = process_dict['process']
+    #     process = process_dict["process"]
     #     # If process seems dead, spawn a new one
     #     if process.stdin.closed or process.stdout.closed or process.poll():
     #         util.system.kill_process(process)
     #         process = swenerstart("", encoding, verbose=False)
-    #         process_dict['process'] = process
+    #         process_dict["process"] = process
 
     # Get sentence annotation
-    sentence = util.get_children(text, None, sentence, token, orphan_alert=True)
+    sentences, orphans = util.get_children(doc, sentence, token, orphan_alert=True)
 
     # Collect all text
-    sentences = [sent for _, sent in sentence.items()]
-    word_file = util.read_annotation(word)
-    stdin = SENT_SEP.join(TOK_SEP.join(word_file[tokid] for tokid in sent)
+    word_annotation = list(util.read_annotation(doc, word))
+    stdin = SENT_SEP.join(TOK_SEP.join(word_annotation[token_index] for token_index in sent)
                           for sent in sentences)
     # Escape <, > and &
     stdin = xml.sax.saxutils.escape(stdin)
@@ -44,7 +42,7 @@ def tag_ne(out_ne_ex, out_ne_type, out_ne_subtype, out_ne_name, word, sentence, 
     # util.log.info("Stdin length: %s, keep process: %s", len(stdin), keep_process)
 
     # if process_dict is not None:
-    #     process_dict['restart'] = not keep_process
+    #     process_dict["restart"] = not keep_process
 
     # # Does not work as of now since swener does not have an interactive mode
     # if keep_process:
@@ -57,19 +55,23 @@ def tag_ne(out_ne_ex, out_ne_type, out_ne_subtype, out_ne_name, word, sentence, 
     # else:
     # Otherwise use communicate which buffers properly
     # util.log.info("STDIN %s %s", type(stdin.encode(encoding)), stdin.encode(encoding))
-    stdout, _ = process.communicate(stdin.encode(encoding))
+    stdout, _ = process.communicate(stdin.encode(util.UTF8))
     # util.log.info("STDOUT %s %s", type(stdout.decode(encoding)), stdout.decode(encoding))
 
-    parse_swener_output(sentences, stdout.decode(encoding), out_ne_ex, out_ne_type, out_ne_subtype, out_ne_name)
+    parse_swener_output(doc, sentences, token, stdout.decode(util.UTF8), out_ne, out_ne_ex, out_ne_type, out_ne_subtype,
+                        out_ne_name)
 
 
-def parse_swener_output(sentences, output, out_ne_ex, out_ne_type, out_ne_subtype, out_ne_name):
+def parse_swener_output(doc, sentences, token, output, out_ne, out_ne_ex, out_ne_type, out_ne_subtype, out_ne_name):
     """Parse the SweNER output and write annotation files."""
 
-    out_ex_dict = {}
-    out_type_dict = {}
-    out_subtype_dict = {}
-    out_name_dict = {}
+    out_ne_spans = []
+    out_ex = []
+    out_type = []
+    out_subtype = []
+    out_name = []
+
+    token_spans = list(util.read_annotation_spans(doc, token))
 
     # Loop through the NE-tagged sentences and parse each one with ElemenTree
     for sent, tagged_sent in zip(sentences, output.strip().split(SENT_SEP)):
@@ -77,45 +79,46 @@ def parse_swener_output(sentences, output, out_ne_ex, out_ne_type, out_ne_subtyp
 
         # Filter out tags on the format <EnamexXxxXxx> since they seem to always overlap with <ENAMEX> elements,
         # making the XML invalid.
-        xml_sent = re.sub(r'</?Enamex[^>\s]+>', '', xml_sent)
+        xml_sent = re.sub(r"</?Enamex[^>\s]+>", "", xml_sent)
         try:
             root = etree.fromstring(xml_sent)
         except:
             util.log.warning("Error parsing sentence. Skipping.")
             continue
 
-        # Init token counter; needed to get start_id and end_id
+        # Init token counter; needed to get start_pos and end_pos
         i = 0
         previous_end = 0
         children = list(root.iter())
 
         try:
-
             for count, child in enumerate(children):
-                start_id = util.edgeStart(sent[i])
+                start_pos = token_spans[sent[i]][0]
                 start_i = i
 
                 # If current child has text, increase token counter
                 if child.text:
                     i += len(child.text.strip().split(TOK_SEP))
 
-                    # Extract NE tags and save them in dictionaries
+                    # Extract NE tags and save them in lists
                     if child.tag != "sroot":
                         if start_i < previous_end:
                             pass
                             # util.log.warning("Overlapping NE elements found; discarding one.")
                         else:
-                            end_id = util.edgeEnd(sent[i - 1])
+                            end_pos = token_spans[sent[i - 1]][1]
                             previous_end = i
-                            edge = util.mkEdge('ne', [start_id, end_id])
-                            out_ex_dict[edge] = child.tag
-                            out_type_dict[edge] = child.get("TYPE")
-                            out_subtype_dict[edge] = child.get("SBT")
-                            out_name_dict[edge] = child.text
+                            span = (start_pos, end_pos)
+                            out_ne_spans.append(span)
+                            out_ex.append(child.tag)
+                            out_type.append(child.get("TYPE"))
+                            out_subtype.append(child.get("SBT"))
+                            out_name.append(child.text)
 
-                        # If this child has a tail and it doesn't start with a space, or if it has no tail at all despite not being the last child,
-                        # it means this NE ends in the middle of a token.
-                        if (child.tail and child.tail.strip() and not child.tail[0] == " ") or (not child.tail and count < len(children) - 1):
+                        # If this child has a tail and it doesn't start with a space, or if it has no tail at all
+                        # despite not being the last child, it means this NE ends in the middle of a token.
+                        if (child.tail and child.tail.strip() and not child.tail[0] == " ") or (
+                                not child.tail and count < len(children) - 1):
                             i -= 1
                             # util.log.warning("Split token returned by name tagger.")
 
@@ -123,7 +126,8 @@ def parse_swener_output(sentences, output, out_ne_ex, out_ne_type, out_ne_subtyp
                 if child.tail and child.tail.strip():
                     i += len(child.tail.strip().split(TOK_SEP))
 
-                if (child.tag == "sroot" and child.text and not child.text[-1] == " ") or (child.tail and not child.tail[-1] == " "):
+                if (child.tag == "sroot" and child.text and not child.text[-1] == " ") or (
+                        child.tail and not child.tail[-1] == " "):
                     # The next NE would start in the middle of a token, so decrease the counter by 1
                     i -= 1
         except IndexError:
@@ -131,10 +135,11 @@ def parse_swener_output(sentences, output, out_ne_ex, out_ne_type, out_ne_subtyp
             continue
 
     # Write annotations
-    util.write_annotation(out_ne_ex, out_ex_dict)
-    util.write_annotation(out_ne_type, out_type_dict)
-    util.write_annotation(out_ne_subtype, out_subtype_dict)
-    util.write_annotation(out_ne_name, out_name_dict)
+    util.write_annotation(doc, out_ne, out_ne_spans)
+    util.write_annotation(doc, out_ne_ex, out_ex)
+    util.write_annotation(doc, out_ne_type, out_type)
+    util.write_annotation(doc, out_ne_subtype, out_subtype)
+    util.write_annotation(doc, out_ne_name, out_name)
 
 
 def swenerstart(stdin, encoding, verbose):
@@ -142,5 +147,5 @@ def swenerstart(stdin, encoding, verbose):
     return util.system.call_binary("hfst-swener", [], stdin, encoding=encoding, verbose=verbose, return_command=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     util.run.main(tag_ne)
