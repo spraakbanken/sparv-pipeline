c6856572200b311e3a4c0c874163db81bcac0df3
Martin Hammarstedt <martin.hammarstedt@gu.se>
2020-03-19 11:41:57 +0100

    Fix read_annotation when reading multiple annotations, and enforce sorting of spans

--- sparv/util/corpus.py
+++ sparv/util/corpus.py
@@ -20,7 +20,6 @@ def annotation_exists(doc, annotation):
 
 def clear_annotation(doc, annotation):
     """Remove an annotation file if it exists."""
-    elem, attr = split_annotation(annotation)
     annotation_path = get_annotation_path(doc, annotation)
     if os.path.exists(annotation_path):
         os.remove(annotation_path)
@@ -39,12 +38,12 @@ def write_annotation(doc, annotation, values, append=False):
         elem_attrs = dict(split_annotation(ann) for ann in annotation)
         # Handle multiple annotations used as one
         assert all(elem_attrs.values()), "Span annotations can not be written while treating multiple annotations as one."
-        # Get spans and associated names for annotations
+        # Get spans and associated names for annotations. We need this information to figure out which value goes to
+        # which annotation.
         spans = read_annotation(doc, elem_attrs.keys(), with_annotation_name=True)
         annotation_values = {elem: [] for elem in elem_attrs.keys()}
 
-        for value in values:
-            _, annotation_name = next(spans)
+        for value, (_, annotation_name) in zip(values, spans):
             annotation_values[annotation_name].append(value)
 
         for annotation_name in annotation_values:
@@ -55,6 +54,10 @@ def write_annotation(doc, annotation, values, append=False):
 def _write_single_annotation(doc, annotation, values, append):
     """Write an annotation to a file."""
     is_span = not split_annotation(annotation)[1]
+
+    if is_span:
+        # Make sure that spans are sorted
+        assert all(values[i] <= values[i + 1] for i in range(len(values) - 1)), "Annotation spans must be sorted."
     file_path = get_annotation_path(doc, annotation)
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     mode = "a" if append else "w"
@@ -74,8 +77,8 @@ def _write_single_annotation(doc, annotation, values, append):
                 end_subpos = ".{}".format(end_subpos) if end_subpos else ""
                 value = "{}{}-{}{}".format(start, start_subpos, end, end_subpos)
             else:
-                # value = value.replace("\\", r"\\").replace("\n", r"\n").replace("\r", "")  # Use if we allow linebreaks in tokens
-                value = value.replace("\n", "").replace("\r", "")  # Don't allow linebreaks in tokens
+                # value = value.replace("\\", r"\\").replace("\n", r"\n").replace("\r", "")  # Use if we allow linebreaks in values
+                value = value.replace("\n", "").replace("\r", "")  # Don't allow linebreaks in values
             print(value, file=f)
             ctr += 1
     # Update file modification time even if nothing was written
@@ -118,8 +121,20 @@ def read_annotation(doc, annotation, with_annotation_name=False):
         yield from _read_single_annotation(doc, annotation[0], with_annotation_name)
     else:
         # Handle multiple annotations used as one
-        yield from heapq.merge(*[_read_single_annotation(doc, ann, with_annotation_name)
-                                 for ann in annotation])
+
+        # Make sure we don't have multiple attributes on the same annotation
+        assert len(annotation) == len(set(split_annotation(ann)[0]
+                                          for ann in annotation)), "Reading multiple attributes on the same " \
+                                                                   "annotation is not allowed."
+
+        # Get iterators for all annotations
+        all_annotations = {split_annotation(ann)[0]: _read_single_annotation(doc, ann, with_annotation_name)
+                           for ann in annotation}
+
+        # We need to read the annotation spans to be able to interleave the values in the correct order
+        for _, ann in heapq.merge(*[_read_single_annotation(doc, split_annotation(ann)[0], with_annotation_name=True)
+                                    for ann in annotation]):
+            yield next(all_annotations[ann])
 
 
 def read_annotation_attributes(doc, annotations, with_annotation_name=False):
@@ -141,7 +156,7 @@ def _read_single_annotation(doc, annotation, with_annotation_name):
             value = line.rstrip("\n\r")
             if not split_annotation(annotation)[1]:  # If this is a span annotation
                 value = tuple(tuple(map(int, pos.split("."))) for pos in value.split("-"))
-            # value = re.sub(r"((?<!\\)(?:\\\\)*)\\n", "\1\n", value).replace(r"\\", "\\")  # Replace literal "\n" with linebreak (only needed if we allow "\n" in tokens)
+            # value = re.sub(r"((?<!\\)(?:\\\\)*)\\n", "\1\n", value).replace(r"\\", "\\")  # Replace literal "\n" with linebreak (only needed if we allow "\n" in values)
             yield value if not with_annotation_name else (value, annotation)
             ctr += 1
     log.info("Read %d items: %s/%s", ctr, doc, annotation)
