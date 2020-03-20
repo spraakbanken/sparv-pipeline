bd99afb22705ae2366b3f931501de81a9e3ff79e
Martin Hammarstedt <martin.hammarstedt@gu.se>
2020-03-20 11:41:12 +0100

    Update number.py to use new annotation format

--- sparv/modules/misc/number.py
+++ sparv/modules/misc/number.py
@@ -7,64 +7,57 @@ from binascii import hexlify
 START_DEFAULT = 1
 
 
-def number_by_position(out, texts, chunks, prefix="", start=START_DEFAULT):
-    """ Number chunks by their position. """
-    if isinstance(chunks, str):
-        chunks = chunks.split()
-    if isinstance(texts, str):
-        texts = texts.split()
-    assert len(chunks) == len(texts), "You must supply the same number of texts and chunks."
+def number_by_position(doc, out, chunk, prefix="", start=START_DEFAULT):
+    """Number chunks by their position."""
 
-    anchors = []
-    for text in texts:
-        _txt, anchor2pos, _pos2anchor = util.corpus.read_corpus_text(text)
-        anchors.append(anchor2pos)
+    spans = list(util.read_annotation_spans(doc, chunk))
 
-    def order(chunknr, edge, _value):
-        value = anchors[chunknr][util.edgeStart(edge)]  # Position in corpus
-        return (chunknr, value)
+    def order(index, _value):
+        return spans[index]
 
-    read_chunks_and_write_new_ordering(out, chunks, order, prefix, start)
+    read_chunks_and_write_new_ordering(doc, out, chunk, order, prefix, start)
 
 
-def number_by_random(out, chunks, prefix="", start=START_DEFAULT):
-    """ Number chunks randomly. """
-    def order(chunknr, edge, _value):
-        random.seed(int(hexlify(edge.encode()), 16))
-        return (chunknr, random.random())
+def number_by_random(doc, out, chunk, prefix="", start=START_DEFAULT):
+    """Number chunks randomly.
+    Uses index as random seed."""
+    def order(index, _value):
+        random.seed(int(hexlify(str(index).encode()), 16))
+        return random.random()
 
-    read_chunks_and_write_new_ordering(out, chunks, order, prefix, start)
+    read_chunks_and_write_new_ordering(doc, out, chunk, order, prefix, start)
 
 
-def renumber_by_attribute(out, chunks, prefix="", start=START_DEFAULT):
-    """ Renumber chunks, with the order determined by an attribute. """
-    def order(_chunknr, _edge, value):
+def renumber_by_attribute(doc, out, chunk, prefix="", start=START_DEFAULT):
+    """Renumber chunks, with the order determined by an attribute."""
+    def order(_index, value):
         return natural_sorting(value)
 
-    read_chunks_and_write_new_ordering(out, chunks, order, prefix, start)
+    read_chunks_and_write_new_ordering(doc, out, chunk, order, prefix, start)
 
 
-def renumber_by_shuffle(out, chunks, prefix="", start=START_DEFAULT):
-    """ Renumber already numbered chunks, in new random order.
-        Retains the connection between parallelly numbered chunks by using the values as random seed. """
-    def order(_chunknr, _edge, value):
+def renumber_by_shuffle(doc, out, chunk, prefix="", start=START_DEFAULT):
+    """Renumber already numbered chunks, in new random order.
+       Retains the connection between parallelly numbered chunks by using the values as random seed."""
+    def order(_index, value):
         random.seed(int(hexlify(value.encode()), 16))
         return random.random(), natural_sorting(value)
 
-    read_chunks_and_write_new_ordering(out, chunks, order, prefix, start)
+    read_chunks_and_write_new_ordering(doc, out, chunk, order, prefix, start)
 
 
-def number_by_parent(out, chunks, parent_order, parent_children, prefix="", start=START_DEFAULT):
-    """ Number chunks by (parent order, chunk order). """
-    PARENT_CHILDREN = util.read_annotation(parent_children)
-    CHILD_ORDER = dict((cid, (pnr, cnr))
-                       for (pid, pnr) in util.read_annotation_iteritems(parent_order)
-                       for (cnr, cid) in enumerate(PARENT_CHILDREN.get(pid, "").split()))
+def number_by_parent(doc, out, chunk, parent_order, prefix="", start=START_DEFAULT):
+    """Number chunks by (parent order, chunk order)."""
+    parent_children, _orphans = util.get_children(doc, parent_order, chunk)
 
-    def order(chunknr, edge, _value):
-        return (chunknr, CHILD_ORDER.get(edge))
+    child_order = {child_index: (parent_nr, child_index)
+                   for parent_index, parent_nr in enumerate(util.read_annotation(doc, parent_order))
+                   for child_index in parent_children[parent_index]}
 
-    read_chunks_and_write_new_ordering(out, chunks, order, prefix, start)
+    def order(index, _value):
+        return child_order.get(index)
+
+    read_chunks_and_write_new_ordering(doc, out, chunk, order, prefix, start)
 
 
 def number_relative(doc, out, parent, child, prefix="", start=START_DEFAULT):
@@ -76,29 +69,34 @@ def number_relative(doc, out, parent, child, prefix="", start=START_DEFAULT):
                                      for cnr, _index in enumerate(parent, start)))
 
 
-def read_chunks_and_write_new_ordering(out, chunks, order, prefix="", start=START_DEFAULT):
-    if isinstance(chunks, str):
-        chunks = chunks.split()
-
+def read_chunks_and_write_new_ordering(doc, out, chunk, order, prefix="", start=START_DEFAULT):
+    """Common function called by other numbering functions."""
     new_order = defaultdict(list)
-    for chunknr, chunk in enumerate(chunks):
-        for edge, val in util.read_annotation_iteritems(chunk):
-            val = order(chunknr, edge, val)
-            new_order[val].append(edge)
+
+    in_annotation = list(util.read_annotation(doc, chunk))
+
+    for i, val in enumerate(in_annotation):
+        val = order(i, val)
+        new_order[val].append(i)
+
+    out_annotation = util.create_empty_attribute(doc, in_annotation)
 
     nr_digits = len(str(len(new_order) - 1 + start))
-    util.write_annotation(out, ((edge, "%s%0*d" % (prefix, nr_digits, nr))
-                                for nr, key in enumerate(sorted(new_order), start)
-                                for edge in new_order[key]))
+    for nr, key in enumerate(sorted(new_order), start):
+        for index in new_order[key]:
+            out_annotation[index] = "%s%0*d" % (prefix, nr_digits, nr)
+
+    util.write_annotation(doc, out, out_annotation)
 
 
 def natural_sorting(astr):
-    return tuple(int(s) if s.isdigit() else s for s in re.split(r'(\d+)', astr))
+    """Convert a string into a naturally sortable tuple."""
+    return tuple(int(s) if s.isdigit() else s for s in re.split(r"(\d+)", astr))
 
 
 ######################################################################
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     util.run.main(position=number_by_position,
                   random=number_by_random,
                   attribute=renumber_by_attribute,
