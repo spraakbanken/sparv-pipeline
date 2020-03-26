9e517ff3ad692109695d10dc8aa898612afaf4f8
Anne Schumacher <anne.schumacher@gu.se>
2020-03-26 12:12:11 +0100

    some improvements in util.export

--- sparv/util/export.py
+++ sparv/util/export.py
@@ -12,8 +12,14 @@ def gather_annotations(doc, annotations):
     for annotation_pointer in annotations:
         span_name, attr = corpus.split_annotation(annotation_pointer)
         if span_name not in annotation_dict:
-            annotation_dict[span_name]["@span"] = []
-            for i, s in enumerate(corpus.read_annotation_spans(doc, span_name)):
+            # This is necessary, span_name needs to be in the dictionary
+            annotation_dict[span_name]["@span"] = None
+            for i, s in enumerate(corpus.read_annotation_spans(doc, span_name, decimals=False)):
+                # TODO: sub positions need to be considered...
+                # start_pos, end_pos = s
+                # start_pos = float(".".join(str(ele) for ele in s[0]))
+                # end_pos = float(".".join(str(ele) for ele in s[1]))
+                # spans_list.append(((start_pos, end_pos), span_name, i))
                 spans_list.append((s, span_name, i))
         if attr and not annotation_dict[span_name].get(attr):
             a = list(corpus.read_annotation(doc, annotation_pointer))
@@ -30,8 +36,8 @@ def gather_annotations(doc, annotations):
         3. the calculated element hierarchy
         """
         pos, name, _i = span
-        hierarchy = elem_hierarchy.index(name) if name in elem_hierarchy else -1
-        return (pos[0], - pos[1], hierarchy)
+        hierarchy_index = elem_hierarchy.index(name) if name in elem_hierarchy else -1
+        return (pos[0], - pos[1], hierarchy_index)
 
     sorted_spans = sorted(spans_list, key=sort_spans)
     return sorted_spans, annotation_dict
@@ -52,21 +58,28 @@ def calculate_element_hierarchy(doc, spans_list):
     # Flatten structure
     unclear_spans = set([elem for elem_set in span_duplicates for elem in elem_set])
 
+    # Read all annotation spans for quicker access later
+    read_items = {}
+    for span in unclear_spans:
+        read_items[span] = sorted(enumerate(corpus.read_annotation_spans(doc, span, decimals=True)), key=lambda x: x[1])
+
     # Get pairs of relations that need to be ordered
     relation_pairs = list(combinations(unclear_spans, r=2))
     # Order each pair into [parent, children]
     ordered_pairs = set()
     for a, b in relation_pairs:
-        a_parent = len([i for i in (parent.get_parents(doc, a, b)) if i is not None])
-        b_parent = len([i for i in (parent.get_parents(doc, b, a)) if i is not None])
+        a_parent = len([i for i in (parent.get_parents(doc, read_items[a], read_items[b])) if i is not None])
+        b_parent = len([i for i in (parent.get_parents(doc, read_items[b], read_items[a])) if i is not None])
         if a_parent > b_parent:
             ordered_pairs.add((a, b))
         else:
             ordered_pairs.add((b, a))
 
     hierarchy = []
+    error_msg = "Something seems to go wrong when sorting annotation elements. Could there be circular relations?"
     # Loop until all unclear_spans are processed
     while unclear_spans:
+        size = len(unclear_spans)
         for span in unclear_spans.copy():
             # Span is never a child in ordered_pairs, then it is first in the hierarchy
             if not any([b == span for _a, b in ordered_pairs]):
@@ -76,4 +89,6 @@ def calculate_element_hierarchy(doc, spans_list):
                 for pair in ordered_pairs.copy():
                     if pair[0] == span:
                         ordered_pairs.remove(pair)
+        # Check that unclear_spans is getting smaller, otherwise there might be circularity
+        assert len(unclear_spans) == size - 1, error_msg
     return hierarchy
