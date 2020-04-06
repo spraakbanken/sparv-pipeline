4e56b0be22324e964ac63453f7dbc7093bc37e8f
Anne Schumacher <anne.schumacher@gu.se>
2020-04-06 17:08:25 +0200

    slight refactoring of split_tuples_list

--- sparv/util/misc.py
+++ sparv/util/misc.py
@@ -24,15 +24,13 @@ def split(value):
 
 
 def split_tuples_list(value):
-    """If 'value' is a string, split and return a list, otherwise return as is.
+    """Convert value to a list containing tuples.
 
-    Furthermore each list item will be split into a tuple by the '>' character.
+    Each list item will be split into a tuple by the '>' character.
     Each tuple will contain 2 elements. If there is no '>' in the string, the second element will be None.
     """
-    if isinstance(value, str):
-        value = value.split()
-        value = [(v.split(">")[0], v.split(">")[1]) if len(tuple(v.split(">"))) > 1 else (v, None) for v in value]
-    elif isinstance(value, list) and isinstance(value[0], str):
+    value = split(value)
+    if isinstance(value, list) and isinstance(value[0], str):
         value = [(v.split(">")[0], v.split(">")[1]) if len(tuple(v.split(">"))) > 1 else (v, None) for v in value]
     return value
 
