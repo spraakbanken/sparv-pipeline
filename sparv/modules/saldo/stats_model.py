e4fd0dbab6a50b49808e6331f959f5315fea633d
Martin Hammarstedt <martin.hammarstedt@gu.se>
2020-05-15 21:00:39 +0200

    Speed up stats model creation by not downloading more data than needed

--- sparv/modules/saldo/stats_model.py
+++ sparv/modules/saldo/stats_model.py
@@ -1,7 +1,9 @@
 """Train a probability model on a Korp statistics file."""
 
 import logging
+import os
 import pickle
+import urllib.request
 
 from nltk import FreqDist, LidstoneProbDist
 
@@ -10,47 +12,74 @@ from sparv import Model, ModelOutput, modelbuilder
 
 log = logging.getLogger(__name__)
 
+MIN_FREQ = 4
+
 
 @modelbuilder("Korp statistic model", language=["swe"])
 def build_korp_stats(out: str = ModelOutput("saldo/stats.pickle"),
-                     saldom: str = Model("saldo/saldom.xml")):
+                     _saldom: str = Model("saldo/saldom.xml")):
     """Download Korp's word frequency file and convert it to a model."""
     txt_path = "saldo/stats_all.txt"
-    log.info("Downloading Korp stats file...")
-    util.download_model("https://svn.spraakdata.gu.se/sb-arkiv/pub/frekvens/stats_all.txt", txt_path)
+    try:
+        log.info("Downloading Korp stats file...")
+        download_stats_file("https://svn.spraakdata.gu.se/sb-arkiv/pub/frekvens/stats_all.txt", txt_path)
 
-    log.info("Building frequency model...")
-    make_model(util.get_model_path(txt_path), out)
+        log.info("Building frequency model...")
+        make_model(util.get_model_path(txt_path), out)
+    finally:
+        # Clean up
+        util.remove_model_files([txt_path])
 
-    # Clean up
-    util.remove_model_files([txt_path])
+
+def download_stats_file(url, destination):
+    """Download statistics file in chunks, aborting when we've got what we need."""
+    destination = util.get_model_path(destination)
+    os.makedirs(os.path.dirname(destination), exist_ok=True)
+
+    response = urllib.request.urlopen(url)
+    chunk_size = 512 * 1024
+    with open(destination, "wb") as out_file:
+        while True:
+            chunk = response.read(chunk_size)
+            if not chunk:
+                break
+            try:
+                # Pick out second line in chunk (first is likely incomplete)
+                first_line = chunk.decode("UTF-8").split("\n", 2)[1]
+                if int(first_line.split("\t")[4]) < MIN_FREQ:
+                    break
+            except UnicodeDecodeError:
+                # Some chunks won't be decodable but that's okay
+                pass
+
+            out_file.write(chunk)
 
 
-def make_model(stats_infile, picklefile, smoothingparam=0.001, min_freq=3, protocol=-1):
+def make_model(stats_infile, picklefile, smoothingparam=0.001, min_freq=MIN_FREQ, protocol=-1):
     """Train a probability model on a Korp statistics file and save it as a pickle file.
 
     The model is a LidstoneProbDist (NLTK) which has tuples (wordform, MSD-tag) as keys
     and smoothed probabilities as values.
     """
     fdist = FreqDist()
-    with open(stats_infile, encoding='utf-8') as f:
+    with open(stats_infile, encoding="UTF-8") as f:
         for line in f:
-            fields = line[:-1].split('\t')
+            fields = line[:-1].split("\t")
             word = fields[0]
+            freq = int(fields[4])
             # Skip word forms that occur fewer times than min_freq
-            if int(fields[4]) < min_freq:
+            if freq < min_freq:
                 break
-            # Get rid of all urls
-            if word.startswith("http://"):
+            # Get rid of all URLs
+            if word.startswith("http://") or word.startswith("https://") or word.startswith("www."):
                 continue
             # # Words that only occur once may only contain letters and hyphens
-            # if fields[4] == '1' and any(not (c.isalpha() or c == "-") for c in word):
+            # if fields[4] == "1" and any(not (c.isalpha() or c == "-") for c in word):
             #     continue
             # if len(word) > 100:
             #     continue
-            simple_msd = fields[1][:fields[1].find('.')] if '.' in fields[1] else fields[1]
-            fdist[(word, simple_msd)] += int(fields[4])
-
+            simple_msd = fields[1][:fields[1].find(".")] if "." in fields[1] else fields[1]
+            fdist[(word, simple_msd)] += freq
     pd = LidstoneProbDist(fdist, smoothingparam, fdist.B())
 
     # Save probability model as pickle
