e175d0512282eadf373b5ed11264274d91ac63c0
Anne Schumacher <anne.schumacher@gu.se>
2020-07-10 13:38:42 +0200

    add test for stanford parser

--- tests/test_annotations.py
+++ tests/test_annotations.py
@@ -5,8 +5,10 @@ import pathlib
 import pytest
 
 import utils
+from sparv.core import paths
 
-@pytest.mark.skip
+
+@pytest.mark.swe
 def test_standard_swe(tmp_path):
     """Run corpus standard-swe and compare the annotations and exports to gold standard."""
     gold_corpus_dir = pathlib.Path("tests/test_corpora/standard-swe")
@@ -26,18 +28,39 @@ def test_standard_swe(tmp_path):
     utils.cmp_export(gold_corpus_dir, test_corpus_dir)
 
 
+@pytest.mark.freeling
 @pytest.mark.skipif(not utils.is_program("analyze"), reason="FreeLing is not installed")
-def test_freeling1(tmp_path):
+def test_freeling_eng_slevel(tmp_path):
     """Run corpus freeling-eng-slevel and compare the annotations and exports to gold standard."""
     gold_corpus_dir = pathlib.Path("tests/test_corpora/freeling-eng-slevel")
     test_corpus_dir = utils.run_sparv(gold_corpus_dir, tmp_path, targets=[
-        "xml_export:pretty",
         "csv_export:csv",
         "cwb:info",
         "cwb:vrt",
         "cwb:vrt_scrambled",
         "korp:timespan_sql",
-        "stats_export:freq_list_simple"
+        "stats_export:freq_list_simple",
+        "xml_export:pretty",
+        "xml_export:preserved_format",
+    ])
+    utils.cmp_annotations(gold_corpus_dir, test_corpus_dir)
+    utils.cmp_export(gold_corpus_dir, test_corpus_dir)
+
+
+@pytest.mark.stanford
+@pytest.mark.skipif(not paths.get_bin_path("stanford_parser").exists(), reason="Stanford Parser is not available")
+def test_stanford_eng(tmp_path):
+    """Run corpus stanford-eng and compare the annotations and exports to gold standard."""
+    gold_corpus_dir = pathlib.Path("tests/test_corpora/stanford-eng")
+    test_corpus_dir = utils.run_sparv(gold_corpus_dir, tmp_path, targets=[
+        # "korp:timespan_sql", # TODO: These are not working due to a bug
+        # "cwb:info",
+        "csv_export:csv",
+        "cwb:vrt",
+        "cwb:vrt_scrambled",
+        "stats_export:freq_list_simple",
+        "xml_export:pretty",
+        "xml_export:preserved_format",
     ])
     utils.cmp_annotations(gold_corpus_dir, test_corpus_dir)
     utils.cmp_export(gold_corpus_dir, test_corpus_dir)
