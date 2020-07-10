d92dc9b4ecca81518f33a5b740b5780fcf928261
Anne Schumacher <anne.schumacher@gu.se>
2020-07-10 18:37:03 +0200

    add test for treetagger

--- tests/test_annotations.py
+++ tests/test_annotations.py
@@ -47,6 +47,25 @@ def test_freeling_eng_slevel(tmp_path):
     utils.cmp_export(gold_corpus_dir, test_corpus_dir)
 
 
+@pytest.mark.treetagger
+@pytest.mark.skipif(not paths.get_bin_path("treetagger").exists(), reason="Treetagger is not available")
+def test_treetagger_nld(tmp_path):
+    """Run corpus treetagger-nld and compare the annotations and exports to gold standard."""
+    gold_corpus_dir = pathlib.Path("tests/test_corpora/treetagger-nld")
+    test_corpus_dir = utils.run_sparv(gold_corpus_dir, tmp_path, targets=[
+        # "korp:timespan_sql", # TODO: These are not working due to a bug
+        # "cwb:info",
+        "csv_export:csv",
+        "cwb:vrt",
+        "cwb:vrt_scrambled",
+        "stats_export:freq_list_simple",
+        "xml_export:pretty",
+        "xml_export:preserved_format",
+    ])
+    utils.cmp_annotations(gold_corpus_dir, test_corpus_dir)
+    utils.cmp_export(gold_corpus_dir, test_corpus_dir)
+
+
 @pytest.mark.stanford
 @pytest.mark.skipif(not paths.get_bin_path("stanford_parser").exists(), reason="Stanford Parser is not available")
 def test_stanford_eng(tmp_path):
