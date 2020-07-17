7745d5f5b5f381414eb8dc646837bb08e35a9fdd
Anne Schumacher <anne.schumacher@gu.se>
2020-07-17 16:06:17 +0200

    Create mini-swe test corpus

--- tests/test_annotations.py
+++ tests/test_annotations.py
@@ -10,6 +10,16 @@ from sparv.util.system import find_binary
 
 
 @pytest.mark.swe
+def test_mini_swe(tmp_path):
+    """Run corpus mini-swe and compare the annotations and exports to gold standard."""
+    gold_corpus_dir = pathlib.Path("tests/test_corpora/mini-swe")
+    test_corpus_dir = utils.run_sparv(gold_corpus_dir, tmp_path)
+    utils.cmp_annotations(gold_corpus_dir, test_corpus_dir)
+    utils.cmp_export(gold_corpus_dir, test_corpus_dir)
+
+
+@pytest.mark.swe
+@pytest.mark.slow
 def test_standard_swe(tmp_path):
     """Run corpus standard-swe and compare the annotations and exports to gold standard."""
     gold_corpus_dir = pathlib.Path("tests/test_corpora/standard-swe")
