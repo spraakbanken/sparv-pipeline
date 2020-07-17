61a4e6f1427e80b66bc7e46031f7d41da8745490
Anne Schumacher <anne.schumacher@gu.se>
2020-07-17 15:14:22 +0200

    adapt tests to changes in freeling module

--- tests/test_corpora/freeling-eng-slevel/gold_export/korp_timespan/timespan.sql
+++ tests/test_corpora/freeling-eng-slevel/gold_export/korp_timespan/timespan.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2328649b77d33b400468a331a1054846aa66e38943847684e0715990e5d1e74
+oid sha256:0b2e617b6f8945e2ba04ac5ed11dd9e662ab621e435cd7b7fdfe48ef5fdead33
 size 1295
--- tests/test_corpora/freeling-fra-txt/gold_export/korp_timespan/timespan.sql
+++ tests/test_corpora/freeling-fra-txt/gold_export/korp_timespan/timespan.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5160e2fd836b273ac84e6714119f587f1bf4c64b2ab9fb948ddfea99c838b58a
+oid sha256:ebb832d17bfcc6762e62c53621fe65b5fe88347c1781cbbfb83983146d7d5644
 size 1051
