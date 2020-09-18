20f92ef1743bbdef1e3c14787df3e70532a1267a
Martin Hammarstedt <martin.hammarstedt@gu.se>
2020-09-18 15:54:49 +0200

    Add small test for .txt source

--- tests/test_annotations.py
+++ tests/test_annotations.py
@@ -27,6 +27,15 @@ def test_special_swe(tmp_path):
 
 
 @pytest.mark.swe
+def test_txt_swe(tmp_path):
+    """Run corpus txt-swe and compare the annotations and exports to gold standard."""
+    gold_corpus_dir = pathlib.Path("tests/test_corpora/txt-swe")
+    test_corpus_dir = utils.run_sparv(gold_corpus_dir, tmp_path)
+    utils.cmp_annotations(gold_corpus_dir, test_corpus_dir)
+    utils.cmp_export(gold_corpus_dir, test_corpus_dir)
+
+
+@pytest.mark.swe
 @pytest.mark.slow
 def test_standard_swe(tmp_path):
     """Run corpus standard-swe and compare the annotations and exports to gold standard."""
---
+++ tests/test_corpora/txt-swe/gold_annotations/dokument/segment.token/hunpos.msd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6c20f6782081586ee2ce0c533fa9f9c5b7e887365952008a7877e5411234151a
+size 181
