645dcb3ef59aa033e148d3f3370bfc1606166164
Martin Hammarstedt <martin.hammarstedt@gu.se>
2020-09-18 15:20:03 +0200

    Add new test corpus containing special cases

--- tests/test_annotations.py
+++ tests/test_annotations.py
@@ -18,6 +18,15 @@ def test_mini_swe(tmp_path):
 
 
 @pytest.mark.swe
+def test_special_swe(tmp_path):
+    """Run corpus special-swe and compare the annotations and exports to gold standard."""
+    gold_corpus_dir = pathlib.Path("tests/test_corpora/special-swe")
+    test_corpus_dir = utils.run_sparv(gold_corpus_dir, tmp_path)
+    utils.cmp_annotations(gold_corpus_dir, test_corpus_dir)
+    utils.cmp_export(gold_corpus_dir, test_corpus_dir)
+
+
+@pytest.mark.swe
 @pytest.mark.slow
 def test_standard_swe(tmp_path):
     """Run corpus standard-swe and compare the annotations and exports to gold standard."""
---
+++ tests/test_corpora/special-swe/gold_annotations/dokument/segment.token/hunpos.msd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b2428cd9bf1550dc53b288e1233e0cafdfab2d1e373f9ab62851eed9d0f584c4
+size 327
