03320d9a0b0a2786db199954b971266967c0e2d1
Martin Hammarstedt <martin.hammarstedt@gu.se>
2020-11-13 14:27:20 +0100

    Rename config variable

--- sparv/modules/korp/install_corpus.py
+++ sparv/modules/korp/install_corpus.py
@@ -11,7 +11,7 @@ log = logging.getLogger(__name__)
 
 
 @installer("Install CWB datafiles on remote host", config=[
-    Config("korp.remote_corpus_registry", "", description="CWB registry path on remote host"),
+    Config("korp.remote_cwb_registry", "", description="CWB registry path on remote host"),
     Config("korp.remote_cwb_datadir", "", description="CWB datadir path on remote host"),
     Config("korp.protected", False, description="Whether this corpus should have limited access or not")
 ])
@@ -23,7 +23,7 @@ def install_corpus(corpus: Corpus = Corpus(),
                    datadir: str = Config("cwb.cwb_datadir"),
                    registry: str = Config("cwb.corpus_registry"),
                    target_datadir: str = Config("korp.remote_cwb_datadir"),
-                   target_registry: str = Config("korp.remote_corpus_registry")):
+                   target_registry: str = Config("korp.remote_cwb_registry")):
     """Install CWB datafiles on server, by rsyncing datadir and registry.
 
     If local and remote paths differ, target_datadir and target_registry must be specified.
