dc0bb74d66195aa178e9a27a0d246dfc0693c9f7
Anne Schumacher <anne.schumacher@gu.se>
2020-11-25 16:44:54 +0100

    add lemmatiser to stanza, re-organise module

--- sparv/modules/stanza/__init__.py
+++ sparv/modules/stanza/__init__.py
@@ -1,3 +1,16 @@
-"""Dependency parsing using Stanza."""
+"""POS tagging, lemmatisation and dependency parsing with Stanza."""
 
-from . import stanza
+from sparv import Config
+
+from . import stanza, models
+
+__config__ = [
+    Config("stanza.resources_file", default="stanza/resources.json", description="Stanza resources file"),
+    Config("stanza.lem_model", default="stanza/lem/sv_suc_lemmatizer.pt", description="Stanza lemmatisation model"),
+    Config("stanza.pos_model", default="stanza/pos/full_sv_talbanken_tagger.pt", description="Stanza POS model"),
+    Config("stanza.pretrain_pos_model", default="stanza/pos/full_sv_talbanken.pretrain.pt",
+           description="Stanza pretrain POS model"),
+    Config("stanza.dep_model", default="stanza/dep/sv_talbanken_parser.pt", description="Stanza dependency model"),
+    Config("stanza.pretrain_dep_model", default="stanza/dep/sv_talbanken.pretrain.pt",
+           description="Stanza pretrain dependency model")
+]
--- sparv/modules/stanza/stanza.py
+++ sparv/modules/stanza/stanza.py
@@ -1,21 +1,109 @@
-"""POS tagging and Dependency parsing using Stanza."""
-
-import json
+"""POS tagging, lemmatisation and dependency parsing with Stanza."""
 
 import stanza
 from stanza.models.common.doc import Document
 
 import sparv.util as util
-from sparv import Annotation, Config, Model, ModelOutput, Output, annotator, modelbuilder
+from sparv import Annotation, Model, Output, annotator
 
 logger = util.get_logger(__name__)
 
 
-@annotator("Part-of-speech annotation with morphological descriptions from Stanza", config=[
-           Config("stanza.pos_model", default="stanza/full_sv_talbanken_tagger.pt", description="Stanza POS model"),
-           Config("stanza.pretrain_pos_model", default="stanza/full_sv_talbanken.pretrain.pt",
-                  description="Stanza pretrain POS model"),
-           Config("stanza.resources_file", default="stanza/resources.json", description="Stanza resources file")])
+@annotator("POS, lemma and dependency relations from Stanza", order=1)
+def annotate(out_msd: Output = Output("<token>:stanza.msd", cls="token:msd",
+                                      description="Part-of-speeches with morphological descriptions"),
+             out_pos: Output = Output("<token>:stanza.pos", cls="token:pos", description="Part-of-speech tags"),
+             out_feats: Output = Output("<token>:stanza.ufeats", cls="token:ufeats",
+                                        description="Universal morphological features"),
+             out_baseform: Output = Output("<token>:stanza.baseform", cls="token:baseform",
+                                           description="Baseform from Stanza"),
+             out_dephead: Output = Output("<token>:stanza.dephead", cls="token:dephead",
+                                          description="Positions of the dependency heads"),
+             out_dephead_ref: Output = Output("<token>:stanza.dephead_ref", cls="token:deprel_ref",
+                                              description="Sentence-relative positions of the dependency heads"),
+             out_deprel: Output = Output("<token>:stanza.deprel", cls="token:deprel",
+                                         description="Dependency relations to the head"),
+             word: Annotation = Annotation("<token:word>"),
+             token: Annotation = Annotation("<token>"),
+             sentence: Annotation = Annotation("<sentence>"),
+             pos_model: Model = Model("[stanza.pos_model]"),
+             pos_pretrain_model: Model = Model("[stanza.pretrain_pos_model]"),
+             lem_model: Model = Model("[stanza.lem_model]"),
+             dep_model: Model = Model("[stanza.dep_model]"),
+             dep_pretrain_model: Model = Model("[stanza.pretrain_dep_model]"),
+             resources_file: Model = Model("[stanza.resources_file]")):
+    """Do dependency parsing using Stanza."""
+    sentences, orphans = sentence.get_children(token)
+    sentences.append(orphans)
+    word_list = list(word.read())
+    msd = []
+    pos = []
+    feats = []
+    baseforms = []
+    dephead = []
+    dephead_ref = []
+    deprel = []
+
+    # Format document for stanza: separate tokens by whitespace and sentences by double new lines
+    document = "\n\n".join([" ".join(word_list[i] for i in sent) for sent in sentences])
+    logger.debug(document)
+
+    # Initialize the pipeline
+    nlp = stanza.Pipeline(
+        lang="sv",
+        processors="tokenize,pos,lemma,depparse",  # Comma-separated list of processors to use
+        dir=str(resources_file.path.parent),
+        lemma_model_path=str(lem_model.path),
+        pos_pretrain_path=str(pos_pretrain_model.path),
+        pos_model_path=str(pos_model.path),
+        depparse_pretrain_path=str(dep_pretrain_model.path),
+        depparse_model_path=str(dep_model.path),
+        tokenize_pretokenized=True,  # Assume the text is tokenized by white space and sentence split by newline. Do not run a model.
+        tokenize_no_ssplit=True,     # Disable sentence segmentation
+        verbose=False
+        # depparse_pretagged=True,  # Only run dependency parsing on the document
+    )
+
+    doc = nlp(document)
+    word_count = 0  # Keep track of total word count for 'dephead' attribute
+    for sent in doc.sentences:
+        for word in sent.words:
+            feats_str = util.cwbset(word.feats.split("|") if word.feats else "")
+            # Calculate dephead as position in document
+            dephead_str = str(word.head - 1 + word_count) if word.head > 0 else "-"
+            dephead_ref_str = str(word.head) if word.head > 0 else ""
+            logger.debug(f"word: {word.text}"
+                         f"\tlemma: {word.lemma}"
+                         f"\tmsd: {word.xpos}"
+                         f"\tpos: {word.upos}"
+                         f"\tfeats: {feats_str}"
+                         f"\tdephead_ref: {dephead_ref_str}"
+                         f"\tdephead: {dephead_str}"
+                         f"\tdeprel: {word.deprel}"
+                         f"\thead word: {sent.words[word.head - 1].text if word.head > 0 else 'root'}")
+            msd.append(word.xpos)
+            pos.append(word.upos)
+            feats.append(feats_str)
+            baseforms.append(word.lemma)
+            dephead.append(dephead_str)
+            dephead_ref.append(dephead_ref_str)
+            deprel.append(word.deprel)
+        word_count += len(sent._words)
+
+    if len(word_list) != word_count:
+        raise util.SparvErrorMessage(
+            "Stanza POS tagger did not seem to respect the given tokenisation! Do your tokens contain whitespaces?")
+
+    out_msd.write(msd)
+    out_pos.write(pos)
+    out_feats.write(feats)
+    out_baseform.write(baseforms)
+    out_dephead_ref.write(dephead_ref)
+    out_dephead.write(dephead)
+    out_deprel.write(deprel)
+
+
+@annotator("Part-of-speech annotation with morphological descriptions from Stanza", order=2)
 def msdtag(out_msd: Output = Output("<token>:stanza.msd", cls="token:msd",
                                     description="Part-of-speeches with morphological descriptions"),
            out_pos: Output = Output("<token>:stanza.pos", cls="token:pos", description="Part-of-speech tags"),
@@ -56,7 +144,6 @@ def msdtag(out_msd: Output = Output("<token>:stanza.msd", cls="token:msd",
     for sent in doc.sentences:
         for word in sent.words:
             word_count += 1
-            # Calculate dephead as position in document
             feats_str = util.cwbset(word.feats.split("|") if word.feats else "")
             logger.debug(f"word: {word.text}"
                          f"\tmsd: {word.xpos}"
@@ -75,11 +162,7 @@ def msdtag(out_msd: Output = Output("<token>:stanza.msd", cls="token:msd",
     out_feats.write(feats)
 
 
-@annotator("Dependency parsing using Stanza", config=[
-           Config("stanza.dep_model", default="stanza/sv_talbanken_parser.pt",
-                  description="Stanza dependency model"),
-           Config("stanza.pretrain_dep_model", default="stanza/sv_talbanken.pretrain.pt",
-                  description="Stanza pretrain dependency model")])
+@annotator("Dependency parsing using Stanza", order=2)
 def dep_parse(out_dephead: Output = Output("<token>:stanza.dephead", cls="token:dephead",
                                            description="Positions of the dependency heads"),
               out_dephead_ref: Output = Output("<token>:stanza.dephead_ref", cls="token:deprel_ref",
@@ -137,49 +220,9 @@ def dep_parse(out_dephead: Output = Output("<token>:stanza.dephead", cls="token:
             deprel.append(word.deprel)
         word_count += len(sent._words)
 
-
     out_dephead_ref.write(dephead_ref)
     out_dephead.write(dephead)
     out_deprel.write(deprel)
-
-
-@modelbuilder("Stanza resources file", language=["swe"])
-def stanza_resources_file(resources_file: ModelOutput = ModelOutput("stanza/resources.json")):
-    """Download and unzip the Stanza dependency model."""
-    # Write resources.json file to keep Stanza from complaining
-    res = json.dumps({
-        "sv": {
-            "lang_name": "Swedish",
-            "tokenize": {
-                "orchid": {},
-                "best": {}
-            },
-            "default_processors": {
-                "tokenize": "orchid"
-            },
-            "default_dependencies": {},
-        }})
-    resources_file.write(res)
-
-
-@modelbuilder("Stanza POS-tagging model", language=["swe"])
-def stanza_pos_model(model: ModelOutput = ModelOutput("stanza/full_sv_talbanken_tagger.pt"),
-                     pretrain: ModelOutput = ModelOutput("stanza/full_sv_talbanken.pretrain.pt")):
-    """Download and unzip the Stanza POS-tagging model."""
-    zip_model = Model("stanza/synt_stanza_full.zip")
-    zip_model.download("https://svn.spraakdata.gu.se/sb-arkiv/pub/stanza/morph_stanza_full.zip")
-    zip_model.unzip()
-    zip_model.remove()
-
-
-@modelbuilder("Stanza dependency model", language=["swe"])
-def stanza_dep_model(model: ModelOutput = ModelOutput("stanza/sv_talbanken_parser.pt"),
-                     pretrain: ModelOutput = ModelOutput("stanza/sv_talbanken.pretrain.pt")):
-    """Download and unzip the Stanza dependency model."""
-    zip_model = Model("stanza/synt_stanza_full.zip")
-    zip_model.download("https://svn.spraakdata.gu.se/sb-arkiv/pub/stanza/synt_stanza_full.zip")
-    zip_model.unzip()
-    zip_model.remove()
 
 
 def _build_doc(sentences, word, baseform, msd, feats, ref):
