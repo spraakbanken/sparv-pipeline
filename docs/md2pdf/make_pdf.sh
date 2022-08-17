d3cec14a8db686353f45a750939491d061cc08ee
Anne Schumacher <anne.schumacher@gu.se>
2022-08-17 10:48:29 +0200

    produce additional pdf docs without title page

--- docs/md2pdf/make_pdf.sh
+++ docs/md2pdf/make_pdf.sh
@@ -2,7 +2,7 @@
 # set -x
 
 # Script for creating PDFs from markdown
-# Requires markdown and latex
+# Requires markdown, latex and Python pandocfilters
 
 USER_MANUAL_FILES="
 ../user-manual/quick-start.md
@@ -26,6 +26,23 @@ DEVELOPERS_GUIDE_FILES="
 line=$(grep -F "__version__" ../../sparv/__init__.py) # Find version number
 version=${line#"__version__ = \""} # Remove prefix
 SPARV_VERSION=${version%"\""} # Remove suffix
+
+
+function make_pandoc {
+    # Convert markdown to latex/pdf:
+    # pandoc -t latex -o $1.tex $1.md \
+    # pandoc -t native $1.md \
+    pandoc -t latex -o $1.pdf $1.md \
+    --filter filter.py \
+    -H settings_template.tex `# include in header` \
+    --template template.tex `# use template`  \
+    --toc `# table of contents` \
+    -N `# numbered sections` \
+    -V urlcolor=RoyalBlue `# color links blue` \
+    --listings `# use listings package for LaTeX code blocks`
+    #-V links-as-notes=true `# print links as footnotes` \
+}
+
 
 function make_document {
     # $1: file name (without extension)
@@ -54,19 +71,15 @@ author: |
       cat $f >> $1.md
       echo -e "\n" >> $1.md
     done
+    make_pandoc $1
 
-    # Convert markdown to latex/pdf:
-    # pandoc -t latex -o $1.tex $1.md \
-    # pandoc -t native $1.md \
-    pandoc -t latex -o $1.pdf $1.md \
-    --filter filter.py \
-    -H settings_template.tex `# include in header` \
-    --template template.tex `# use template`  \
-    --toc `# table of contents` \
-    -N `# numbered sections` \
-    -V urlcolor=RoyalBlue `# color links blue` \
-    --listings `# use listings package for LaTeX code blocks`
-    #-V links-as-notes=true `# print links as footnotes` \
+    # Make GU-ISS publication without header
+    for f in $2
+    do
+      cat $f >> GU-ISS-$1.md
+      echo -e "\n" >> GU-ISS-$1.md
+    done
+    make_pandoc GU-ISS-$1
 }
 
 # Make PDFs
@@ -76,3 +89,5 @@ make_document developers-guide "$DEVELOPERS_GUIDE_FILES" "Developer's Guide"
 # Clean-up
 rm user-manual.md
 rm developers-guide.md
+rm GU-ISS-user-manual.md
+rm GU-ISS-developers-guide.md
