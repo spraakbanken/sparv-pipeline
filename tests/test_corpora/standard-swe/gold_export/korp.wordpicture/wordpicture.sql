542a389cc5d0b3104542f93efa86cde77213b62c
Martin Hammarstedt <martin.hammarstedt@gu.se>
2026-04-17 18:23:48 +0200

    Update tests

--- tests/test_corpora/standard-swe/gold_export/korp.wordpicture/wordpicture.sql
+++ tests/test_corpora/standard-swe/gold_export/korp.wordpicture/wordpicture.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4bf9cf54b4452e22cf7f79eb8a6f7fa345cc6c5090cd72b225981061d4d7dde9
-size 53477
+oid sha256:6fd3ee4978df890dec6eeaba6c71510899fe409a451303ee79f5c1eaaf8ebdeb
+size 39993
---
+++ tests/test_corpora/standard-swe/gold_export/korp.wordpicture/wordpicture_strings.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:69d9143d171dfb20f95a7f7275ddaa78ffd33866db5e419b105a6ab2c69c971e
+size 11300
